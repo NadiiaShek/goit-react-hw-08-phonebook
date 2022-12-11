4bea212bf4efbf20ef8be826a3f96659a1e61794
Nadiia <nadiia.shek@gmail.com>
2022-12-11 19:39:36 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -5,16 +5,20 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './style/theme';
 import { GlobalStyle } from './style/GlobalStyle';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+// import {  store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
-          <App />
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+            <App />
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </ThemeProvider>
     <GlobalStyle />
