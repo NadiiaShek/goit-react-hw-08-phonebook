4d04fca6b4db39b476866992f7a4016c47f57d2c
Nadiia <nadiia.shek@gmail.com>
2022-12-12 15:58:17 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -5,20 +5,16 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './style/theme';
 import { GlobalStyle } from './style/GlobalStyle';
 import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store';
+import { store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
-// import {  store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter basename="/goit-react-hw-08-phonebook/">
-            <App />
-          </BrowserRouter>
-        </PersistGate>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+          <App />
+        </BrowserRouter>
       </Provider>
     </ThemeProvider>
     <GlobalStyle />
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,50 +1,10 @@
-// import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer, authReducer } from './contactSlice';
 import { filterReducer } from './filterSlice';
-// export const store = configureStore({
-//   reducer: {
-//     contacts: contactsReducer,
-//     filter: filterReducer,
-//     auth: authReducer,
-//   },
-// });
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-// import { authReducer } from './contactSlice';
-// import contactsSlice from './contacts/contacts-slice';
-
-const middleware = [
-  ...getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
-];
-
-const authPersistConfig = {
-  key: 'auth',
-  storage,
-  whitelist: ['token'],
-};
-
 export const store = configureStore({
   reducer: {
-    auth: persistReducer(authPersistConfig, authReducer),
     contacts: contactsReducer,
     filter: filterReducer,
+    auth: authReducer,
   },
-  middleware,
-  devTools: process.env.NODE_ENV === 'development',
 });
-
-export const persistor = persistStore(store);
